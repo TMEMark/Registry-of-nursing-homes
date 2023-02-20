4b8a4ac4a515051a730f24253fff3d39bf47b0b7
TMEMark <markobuljan.os@gmail.com>
2023-02-20 14:20:50 +0100

    CategoryEntity created as POJO added attributes, generated getters and setters

---
+++ Backend/persistance/entity/CategoryEntity.php
@@ -0,0 +1,21 @@
+<?php
+class CategoryEntity{
+    private String $name;
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return $this->name;
+	}
+	
+	/**
+	 * @param string $name 
+	 * @return self
+	 */
+	public function setName(string $name): self {
+		$this->name = $name;
+		return $this;
+	}
+}
+?>
