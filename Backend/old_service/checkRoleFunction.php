d1bc8aecdab29d7ef88b9177cb8c553bc0a340ba
TMEMark <markobuljan.os@gmail.com>
2023-03-13 00:09:17 +0100

    deleted old files

--- Backend/old_service/checkRoleFunction.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function checkUserRole($userId, $userRole){
-    
-}
---
+++ Backend/rest/request/Location/LocationController.php
@@ -0,0 +1,18 @@
+
+<?php
+
+use mapper\LocationMapper;
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header('Content-type: application/json');
+
+require_once '../../../persistance/dao/LocationDao.php';
+require_once '../../../persistance/mapper/LocationMapper.php';
+require_once '../../../service/LocationService.php';
+require_once '../../../db/DatabaseConnection.php';
+require_once 'LocationReqHandler.php';
+
+$controller = new LocationReqHandler(new LocationService(new LocationDao(new LocationMapper())));
+$controller->handleRequests();
