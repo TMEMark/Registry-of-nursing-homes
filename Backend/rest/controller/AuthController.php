d0a207b8943c8d2f87ca9a0cb6f082424f39189b
TMEMark <markobuljan.os@gmail.com>
2023-06-23 10:57:02 +0200

    added missing file locations to AuthController.php

--- Backend/rest/controller/AuthController.php
+++ Backend/rest/controller/AuthController.php
@@ -5,6 +5,12 @@ use mapper\UserMapper;
 use request\AuthReqHandler;
 use service\AuthService;
 
+require_once '../../db/DatabaseConnection.php';
+require_once '../request/AuthReqHandler.php';
+require_once '../../service/AuthService.php';
+require_once '../../persistance/repository/UserRepository.php';
+require_once '../mapper/UserMapper.php';
+
 $reqHandler = new AuthReqHandler(new AuthService(new UserRepository(new UserMapper())));
 
 $request = $action = $_GET['action'];
