3f82719088b07d902c56693e3a4be777346969e0
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2023-06-29 14:59:56 +0200

    Delete Backend directory

--- Backend/Service/testAdmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../Database/pdo.php';
-include 'getAdminData.php';
-
-$result = chechIfUserExists($conn,1);
-
-while($row  = $result->fetch_assoc()){
-    echo $row;
-}
--- Backend/rest/dto/ServiceProviderTestDto.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace dto;
-
-class ServiceProviderTestDto
-{
-
-}
